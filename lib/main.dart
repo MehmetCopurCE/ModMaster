9a4e805b28e2947a007df2d1e9790e2dc9f70b45
Efsanidis <efsanids@gmail.com>
2023-11-07 17:55:39 +0300

    fjfjf

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // always marked "fina
 
   final String title;
 
