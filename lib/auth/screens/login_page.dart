227f69bf474d81ef4465f34e7cd2e3e2eef0fc70
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2023-12-05 23:57:14 +0300

    Authentication Completed

--- lib/auth/screens/login_page.dart
+++ lib/auth/screens/login_page.dart
@@ -13,6 +13,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool hasAccount = true;
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_project/screens/auth_check.dart';
-import 'auth/screens/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async{
