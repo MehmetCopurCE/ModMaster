d36837758b6bcc9d84d4a198726cdd6544f04139
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2023-12-28 01:35:30 +0300

    authCheck edited

--- lib/screens/auth_check.dart
+++ lib/screens/auth_check.dart
@@ -22,8 +22,7 @@ class AuthCheck extends StatelessWidget {
           String userEmail = user?.email ?? '';
           print('User Email: ${userEmail}');
 
-          // fireStoreService.addRegistersToFirestore(
-          //     registerList, userEmail); // Call startPeriodicUpdate here
+          fireStoreService.addRegistersToFirestore(registerList, userEmail); // Call startPeriodicUpdate here
 
           return BottomNavy(); // Kullanıcı oturum açtıysa ana ekrana yönlendir.
         } else {
