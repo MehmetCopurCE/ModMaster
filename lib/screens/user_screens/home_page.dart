f6eca6e50cb64cd845549fcd1dc9067a101576b7
ebruil <ebruil.1612@gmail.com>
2024-01-02 00:03:19 +0300

    home page ve register page de ufak değişiklikler yapıldı

--- lib/screens/user_screens/home_page.dart
+++ lib/screens/user_screens/home_page.dart
@@ -7,7 +7,7 @@ import 'package:mobile_project/screens/main_page.dart'; // Import the MainPage
 import '../../auth/screens/login_page.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -25,26 +25,58 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  // Function to navigate to MainPage
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Hoş geldiniz!'),
-          const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => const RegistersPage(),
-              ));
-            },
-            child: const Text("Registers"),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.white, // Change the background color
+        ),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Merhaba!',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.indigo, // Change the text color
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Uygulamaya Hoş Geldiniz',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const RegistersPage(),
+                  ));
+                },
+                icon: const Icon(Icons.book),
+                label: const Text('Registers'),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.indigo, // Change the button color
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                ),
+              ),
+              const SizedBox(height: 10), // Add some spacing
+              // You can add more buttons if needed
+            ],
           ),
-          const SizedBox(height: 30),
-        ],
+        ),
       ),
     );
   }
--- lib/screens/user_screens/registers_page.dart
+++ lib/screens/user_screens/registers_page.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:intl/intl.dart';
 import 'package:mobile_project/auth/widgets/login_form.dart';
@@ -11,7 +9,7 @@ import 'package:mobile_project/service/firestore_service.dart';
 import 'package:mobile_project/utils/constants.dart';
 
 class RegistersPage extends StatefulWidget {
-  const RegistersPage({super.key});
+  const RegistersPage({Key? key}) : super(key: key);
 
   @override
   RegistersPageState createState() => RegistersPageState();
@@ -63,31 +61,38 @@ class RegistersPageState extends State<RegistersPage> {
             );
           }
 
-          // "registers" koleksiyonundaki tüm belgeleri al
           var registers = snapshot.data!.docs;
 
           return ListView.builder(
             itemCount: registers.length,
             itemBuilder: (context, index) {
               var registerData =
-                  registers[index].data() as Map<String, dynamic>;
-
-              // Register nesnesini oluştur
+              registers[index].data() as Map<String, dynamic>;
               Register register = Register.fromJson(registerData);
-
-              // Register'ın son değerini al
               RegisterValue latestValue = register.registerValue.last;
 
-              return ListTile(
-                title: Text(register.registerName),
-                subtitle: Text(
-                    'Last Date: ${formatDateTime(latestValue.date)} - Last Value: ${latestValue.value}'),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) =>
-                        RegisterDetailPage(registerName: register.registerName),
-                  ));
-                },
+              return Card(
+                elevation: 5, // Set the elevation for a card effect
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                color: Colors.grey[200],
+                child: ListTile(
+                  title: Text(register.registerName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  subtitle: Text(
+                    'Last Date: ${formatDateTime(latestValue.date)} - Last Value: ${latestValue.value}',
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => RegisterDetailPage(
+                        registerName: register.registerName,
+                      ),
+                    ));
+                  },
+                ),
               );
             },
           );
